4e0d0a4bdf9175229689ee3b2fe5ad21718a4f82
sohyeon-jeon <wjsthgus511@naver.com>
2023-03-01 19:00:31 +0900

    add 다리를 지나는 트럭 in Algorithm/Programmers

---
+++ Algorithm/Programmers/다리를 지나는 트럭.py
@@ -0,0 +1,18 @@
+# from collections import deque
+
+def solution(bridge_length, weight, truck_weights):
+    answer = 0
+
+    bridge=[0 for _ in range(bridge_length)]
+
+    while len(bridge) and len(truck_weights):
+        answer+=1
+        bridge.pop(0)
+        if(truck_weights):
+            if truck_weights[0]+sum(bridge)<=weight:
+                bridge.append(truck_weights.pop(0))
+            else:
+                bridge.append(0)
+    return answer
+
+solution(2, 10, [7,4,5,6])
