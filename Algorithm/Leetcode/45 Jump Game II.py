6a7f154c395beb2d403d6c1e524597cf3a941f1a
wjsthgus511@naver.com <sohyeon-jeon>
2023-07-11 13:29:44 +0900

    jump game

---
+++ Algorithm/Leetcode/45 Jump Game II.py
@@ -0,0 +1,18 @@
+from typing import List
+
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        dp=[0]*len(nums)
+        for i in range(len(nums)):
+            for j in range(1,nums[i]+1):
+                if i+j>=len(nums):
+                    continue
+                else:
+                    # 이미 dp에 값이 있으면 그 값이 최소값이므로, dp[i+j]가 0인 것만 이동시킨다! 
+                    if dp[i+j]==0:
+                        dp[i+j]=dp[i]+1
+        return d[-1]
+
+
+solution=Solution()
+solution.jump([1,2,3])
